efb87e9f5ec0f13e64c7a3e0f47aff4614e6a90b
Kalyna-Serhii <kalyna.serhii@lll.kpi.ua>
2024-12-21 18:06:06 +0200

    LR9

---
+++ src/main/java/utils/clientServer/backend/CourseWorkSpringApplication.java
@@ -0,0 +1,11 @@
+package utils.clientServer.backend;
+import javafx.application.Application;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWorkSpringApplication {
+
+    public static void main(String[] args) {
+        Application.launch(App.class, args);
+    }
+}
